85897bf404dc231e18101d067b2fcf8dde8ebf46
besil <silvio.bernardinello@gmail.com>
2015-07-13 21:50:49 +0200

    Added first crawler implementation

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,6 +1,26 @@
 __author__ = 'besil'
 
-if __name__ == '__main__':
-    print("Hello world!")
+import urllib.request
+import time
 
-    print("Ciao")
+if __name__ == '__main__':
+    url = "http://curia.europa.eu/juris/document/document_print.jsf?doclang=IT&pageIndex=0&part=1&mode=req&docid={}"
+
+    docid = 49331
+    limit = docid + 1000
+
+    while (docid < limit):
+        f = urllib.request.urlopen(url.format(docid))
+
+        outname = "document_{}.html".format(docid)
+
+        data = f.read().decode("utf-8")
+        print("{} -> {}".format(outname, len(data)))
+        if (len(data) > 4610):
+            with open(outname, "w") as out:
+                out.write(data)
+
+        time.sleep(1)
+        docid += 1
+
+        print("Document: {}".format(docid))
