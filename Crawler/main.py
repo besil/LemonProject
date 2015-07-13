4b021b5c78825795682e4ce00c034fc1ce2ab5a1
besil <silvio.bernardinello@gmail.com>
2015-07-13 22:29:06 +0200

    Threaded Refact

--- Crawler/main.py
+++ Crawler/main.py
@@ -8,27 +8,29 @@ from bottle import run, get
 
 url = "http://curia.europa.eu/juris/document/document_print.jsf?doclang=IT&pageIndex=0&part=1&mode=req&docid={}"
 stopped_job = True
-current_id = 49331
 
 
-def crawl():
+def crawl(min_size=5000, start_id=0):
     global stopped_job
-    docid = current_id
+    docid = start_id
+    crawled_docs = 0
 
     while not stopped_job:
+
         f = urllib.request.urlopen(url.format(docid))
         outname = "data/document_{}.html".format(docid)
         data = f.read().decode("utf-8")
 
-        print("{} -> {}: {}".format(outname, len(data), len(data) > 4610))
-        if (len(data) > 4610):
-            print("Saving")
+        # print("{} -> {}: {}".format(outname, len(data), len(data) > 4610))
+        if (len(data) > min_size):
             with open(outname, "w") as out:
                 out.write(data)
                 out.flush()
 
         time.sleep(1)
         docid += 1
+        crawled_docs += 1
+        if crawled_docs % 10 == 0: print("Crawled {} documents".format(crawled_docs))
 
     print("Stopped crawl")
 
@@ -38,7 +40,7 @@ def start_job():
     global stopped_job
     print("Starting job")
     stopped_job = False
-    t = Thread(target=crawl)
+    t = Thread(target=crawl, kwargs={"min_size": 4610, "start_id": 49331})
     t.start()
     print("Job started")
     return "Job started\r\n"
