5e2898ae879e3325ce3ebce4e3d83d097a516731
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2015-07-14 18:22:44 +0200

    Added elasticsearch memorization

--- Crawler/main.py
+++ Crawler/main.py
@@ -7,21 +7,25 @@ stopped_job = True
 def crawl(min_size=5000, start_id=0):
     global stopped_job
     import time, urllib.request
+    from elasticsearch import Elasticsearch
 
     doc_id = start_id
     crawled_docs = 0
 
+    es = Elasticsearch()
     while not stopped_job:
-
         f = urllib.request.urlopen(url.format(doc_id))
         outname = "data/document_{}.html".format(doc_id)
-        data = f.read().decode("utf-8")
+        data = str(f.read().decode("utf-8"))
 
         # print("{} -> {}: {}".format(outname, len(data), len(data) > 4610))
         if len(data) > min_size:
-            with open(outname, "w") as out:
-                out.write(data)
-                out.flush()
+            # with open(outname, "w") as out:
+            # out.write(data)
+            # out.flush()
+            # print(data)
+            doc = {"id": doc_id, "data": data}
+            es.index(index="crawling", doc_type="text", id=doc_id, body=doc)
 
         time.sleep(1)
         doc_id += 1
