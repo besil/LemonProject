3c9449a69b4faa05ee24c8d43cf89a133c2aa739
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2015-07-14 17:22:09 +0200

    Refact

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,17 +1,12 @@
 __author__ = 'besil'
 
-import urllib.request
-import time
-from threading import Thread
-
-from bottle import run, get
 
 url = "http://curia.europa.eu/juris/document/document_print.jsf?doclang=IT&pageIndex=0&part=1&mode=req&docid={}"
 stopped_job = True
 
-
 def crawl(min_size=5000, start_id=0):
     global stopped_job
+    import time, urllib.request
     doc_id = start_id
     crawled_docs = 0
 
@@ -35,26 +30,44 @@ def crawl(min_size=5000, start_id=0):
     print("Stopped crawl")
 
 
-@get("/start")
-def start_job():
-    global stopped_job
-    print("Starting job")
-    stopped_job = False
-    t = Thread(target=crawl, kwargs={"min_size": 4610, "start_id": 49331})
-    t.start()
-    print("Job started")
-    return "Job started\r\n"
+def precheck():
+    import sys
 
+    major_version = sys.version_info[0]
+    if major_version != 3:
+        raise Exception("You must use python3")
+    try:
+        import bottle
+    except ImportError:
+        from subprocess import call
 
-@get("/stop")
-def stop_job():
-    global stopped_job
-    stopped_job = True
-    print("Stopping job")
-    return "Stopping job\r\n"
+        call(["pip3", "install", "bottle"])
 
 if __name__ == '__main__':
-    # Per attivare il job: curl curl http://localhost:8150/start
+    # Per attivare il job: curl http://localhost:8150/start
     # Per stoppare il job: curl http://localhost:8150/stop
+
+    precheck()
+
+    from bottle import run, get
+
+    @get("/start")
+    def start_job():
+        global stopped_job
+        from threading import Thread
+
+        print("Starting job")
+        stopped_job = False
+        t = Thread(target=crawl, kwargs={"min_size": 4610, "start_id": 49331})
+        t.start()
+        print("Job started")
+        return "Job started\r\n"
+
+    @get("/stop")
+    def stop_job():
+        global stopped_job
+        stopped_job = True
+        print("Stopping job")
+        return "Stopping job\r\n"
 
     run(host="localhost", port=8150, debug=True)
