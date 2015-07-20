4852c5a9b0e8895b37cf2f98d8805cdd8e1ca633
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2015-07-20 09:02:17 +0200

    Refact

--- Apiv1/js/searcher.js
+++ Apiv1/js/searcher.js
@@ -3,6 +3,7 @@ angular.module('myApp', []).controller('searcher', function($scope, $http) {
     $scope.query_title = "";
     $scope.results = {};
     $scope.show_results = "false";
+    $scope.remote_url = "http://curia.europa.eu/juris/document/document_print.jsf?doclang=IT&pageIndex=0&part=1&mode=req&docid=";
 
     $scope.search = function() {
         q = $scope.query;
